455a19f276477f16c64cb025d713ec8d7b4a45d0
abhijitsharma <abhijit.sharma@gmail.com>
2021-05-30 20:35:32 +0530

    - refactor cleanup

--- word_puzzle/code/card_shift_shuffle.py
+++ word_puzzle/code/card_shift_shuffle.py
@@ -1,55 +1,65 @@
 import math
 
 
-def shift(p, pile_sz):
-    if p > pile_sz * 2:
-        new_p = p - pile_sz
+def shift(pos, pile_sz):
+    if pos > pile_sz * 2:
+        new_pos = pos - pile_sz
     else:
-        if p <= pile_sz:
-            new_p = p + pile_sz
+        if pos <= pile_sz:
+            new_pos = pos + pile_sz
         else:
-            new_p = p
-    print(f'shif in.p {p} out.p {new_p}')
-    return new_p
+            new_pos = pos
+    print(f'shift   in.p {pos} out.p {new_pos}')
+    return new_pos
 
 
-def shuffle(p, pile_sz, num_piles):
-    q = math.floor(p / num_piles)
-    r = p % num_piles
+def shuffle(pos, pile_sz, num_piles):
+    row, col = shuffled_row_col(pos, num_piles)
+    new_pos = row + ((col - 1) * pile_sz)
+    print(f'shuffle in.p {pos} out.p {new_pos} ({row},{col})')
+    return new_pos
+
+
+def shuffled_row_col(pos, num_piles):
+    q = math.floor(pos / num_piles)
+    r = pos % num_piles
     if r == 0:
         row = q
         col = r + num_piles
     else:
         row = q + 1
         col = r
-    new_p = row + ((col - 1) * pile_sz)
-    print(f'shuf in.p {p} out.p {new_p} ({row},{col})')
-    return new_p
+    return row, col
 
 
 def test():
-
-    # num_piles = 3
-    # pile_sz = 7
-    # num_rounds = 3
-    # for p in range(1, (num_piles * pile_sz + 1)):
-    #     play_round(p, pile_sz, num_piles, num_rounds)
-
-    # num_piles = 3
-    # pile_sz = 5
-    # num_rounds = 3
-    # for p in range(1, (num_piles * pile_sz + 1)):
-    #     play_round(p, pile_sz, num_piles, num_rounds)
-
     num_piles = 3
-    pile_sz = 9
+    pile_sz = 7
     num_rounds = 3
     for p in range(1, (num_piles * pile_sz + 1)):
         play_round(p, pile_sz, num_piles, num_rounds)
 
+    num_piles = 3
+    pile_sz = 5
+    num_rounds = 3
+    for p in range(1, (num_piles * pile_sz + 1)):
+        play_round(p, pile_sz, num_piles, num_rounds)
+
+    # num_piles = 3
+    # pile_sz = 15
+    # num_rounds = 5
+    # for p in range(1, (num_piles * pile_sz + 1)):
+    #     play_round(p, pile_sz, num_piles, num_rounds)
+
+    # num_piles = 5
+    # pile_sz = 7
+    # num_rounds = 12
+    # for p in range(1, (num_piles * pile_sz + 1)):
+    #     play_round(p, pile_sz, num_piles, num_rounds)
+
 
 def play_round(p, pile_sz, num_piles, num_rounds):
-    print(f'in.p {p}')
+    print(f'start.p      {p}')
     for i in range(1, num_rounds + 1):
         p = shift(p, pile_sz)
         p = shuffle(p, pile_sz, num_piles)
