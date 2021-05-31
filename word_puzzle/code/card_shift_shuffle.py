6f3673086c14a9e6239bd7b378fe613b895e075a
abhijitsharma <abhijit.sharma@gmail.com>
2021-05-31 21:34:59 +0530

    bunch of enhancements

--- word_puzzle/code/card_shift_shuffle.py
+++ word_puzzle/code/card_shift_shuffle.py
@@ -12,18 +12,11 @@ def shift(pos, pile_sz, num_piles):
     diff = mid - pile
     new_pos = pos + (diff * pile_sz)
     # print(f'pos {pos} pile {pile} mid {mid} diff {diff} new pos {new_pos}')
-    print(f'shift   in.p {pos} out.p {new_pos}')
+    print(f'shift   in.p {pos} out.p {new_pos} {pos_to_point(new_pos, pile_sz)}')
     return new_pos
 
 
 def shuffle(pos, pile_sz, num_piles):
-    row, col = shuffled_row_col(pos, num_piles)
-    new_pos = row + ((col - 1) * pile_sz)
-    print(f'shuffle in.p {pos} out.p {new_pos} ({row},{col})')
-    return new_pos
-
-
-def shuffled_row_col(pos, num_piles):
     q = math.floor(pos / num_piles)
     r = pos % num_piles
     if r == 0:
@@ -32,27 +25,46 @@ def shuffled_row_col(pos, num_piles):
     else:
         row = q + 1
         col = r
+    new_pos = row + ((col - 1) * pile_sz)
+    print(f'shuffle in.p {pos} out.p {new_pos} ({row}, {col})')
+    return new_pos
+
+
+def pos_to_point(pos, pile_sz):
+    q = math.floor(pos / pile_sz)
+    r = pos % pile_sz
+    if r == 0:
+        row = pile_sz
+        col = q
+    else:
+        row = r
+        col = q + 1
     return row, col
 
 
+def play_round(pos, pile_sz, num_piles, num_rounds):
+    print(f'start.p      {pos} {pos_to_point(pos, pile_sz)}')
+    # midpoint
+    expected = math.floor((pile_sz * num_piles) / 2) + 1
+    for i in range(1, num_rounds + 1):
+        pos = shift(pos, pile_sz, num_piles)
+        pos = shuffle(pos, pile_sz, num_piles)
+    assert pos == expected
+    print("-----------")
+
+
 def test():
-    num_piles = 3
-    pile_sz = 3
-    num_rounds = 3
-    for p in range(1, (num_piles * pile_sz + 1)):
-        play_round(p, pile_sz, num_piles, num_rounds)
-
-    num_piles = 3
-    pile_sz = 7
-    num_rounds = 3
-    for p in range(1, (num_piles * pile_sz + 1)):
-        play_round(p, pile_sz, num_piles, num_rounds)
-
-    num_piles = 3
-    pile_sz = 5
-    num_rounds = 3
-    for p in range(1, (num_piles * pile_sz + 1)):
-        play_round(p, pile_sz, num_piles, num_rounds)
+    # num_piles = 3
+    # pile_sz = 7
+    # num_rounds = 3
+    # for p in range(1, (num_piles * pile_sz + 1)):
+    #     play_round(p, pile_sz, num_piles, num_rounds)
+    #
+    # num_piles = 3
+    # pile_sz = 5
+    # num_rounds = 3
+    # for p in range(1, (num_piles * pile_sz + 1)):
+    #     play_round(p, pile_sz, num_piles, num_rounds)
 
     num_piles = 3
     pile_sz = 9
@@ -67,36 +79,23 @@ def test():
         play_round(p, pile_sz, num_piles, num_rounds)
 
 
-def play_round(p, pile_sz, num_piles, num_rounds):
-    print(f'start.p      {p}')
-    for i in range(1, num_rounds + 1):
-        p = shift(p, pile_sz, num_piles)
-        p = shuffle(p, pile_sz, num_piles)
-    print("-----------")
-
-
 if __name__ == '__main__':
     test()
 
-# 12 => 18
-# 14 => 12
-# 8 => 10
-# 10 => 4
-# 11 => 11
-#
-# p + 7
-# p - 7
-#
-# q = p / 3
-# r = p % 3
-#
-# p = 11, q = 3, r = 2 => (4, 2) => 11
-# p = 8, q = 2, r = 2 => (3, 2) => 10
-# p = 10, q = 3, r = 1 => (4, 1) => 4
-# p = 14, q = 4, r = 2 => (5, 2) => 12
-# p = 12, q = 4, r = 0 => (4, 3) = 18
-#
-# if r == 0 : (row = q, col = r + 3)
-#    else   : (row = q + 1, col = r)
-#
-# row + ((col - 1) * 7)
+# 1 8  15
+# 2 9  16
+# 3 10 17
+# 4 11 18
+# 5 12 19
+# 6 13 20
+# 7 14 21
+
+
+# 8, 10, 11
+# 9, 10, 11
+# 10, 11
+# 11
+# 12, 11
+# 13, 12, 11
+# 14, 12, 11
+
